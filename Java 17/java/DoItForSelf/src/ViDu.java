b13052344f5b6da807c9210130c967348f0a136d
Tai Dao <daotai221192@gmail.com>
2022-11-30 14:31:15 +0700

    up

--- Java 17/java/DoItForSelf/src/ViDu.java
+++ Java 17/java/DoItForSelf/src/ViDu.java
@@ -5,7 +5,6 @@ public class ViDu {
     public static void main(String[] args) {
         for (int i = 0; i < 50; i++) {
             System.out.println("Hello, Java " + (i + 1));
-            sout
         }
     }
 }
