6f58ef4a0ca830403dacf117eb4ed9325a8fcd4e
Tai Dao <daotai221192@gmail.com>
2023-02-01 10:48:02 +0700

    27

--- Java 17/java/leetcode/src/main/java/leetcode/_27_Remove_Element.java
+++ Java 17/java/leetcode/src/main/java/leetcode/_27_Remove_Element.java
@@ -1,41 +1,8 @@
 package leetcode;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class _27_Remove_Element {
 
-    public static int removeElement1(int[] nums, int val) {
-        Integer[] nums2 = new Integer[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            nums2[i] = nums[i];
-        }
-        List<Integer> list = new ArrayList<>(List.of(nums2));
-        list.removeIf(n -> n == val);
-        for (int i = 0; i < list.size(); i++) {
-            nums[i] = list.get(i);
-        }
-        return list.size();
-    }
-
-    public static int removeElement2(int[] a, int val) {
-        int n = a.length;
-
-        for (int i = 0; i < n; ) {
-            if (a[i] == val) {
-                for (int j = i; j < n - 1; j++) {
-                    a[j] = a[j + 1];
-
-                }
-                n--;
-            } else {
-                i++;
-            }
-        }
-
-        return n;
-    }
 
     public static int removeElement(int[] a, int val) {
         int n = a.length;
