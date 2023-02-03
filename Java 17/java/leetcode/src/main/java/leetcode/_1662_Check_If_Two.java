4f65a21fc6940590450c1be3351142b7ec386812
Tai Dao <daotai221192@gmail.com>
2023-02-03 10:04:50 +0700

    Create _1662_Check_If_Two.java

---
+++ Java 17/java/leetcode/src/main/java/leetcode/_1662_Check_If_Two.java
@@ -0,0 +1,9 @@
+package leetcode;
+
+public class _1662_Check_If_Two {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        String s1 = String.join("", word1);
+        String s2 = String.join("", word2);
+        return s1.equals(s2);
+    }
+}
