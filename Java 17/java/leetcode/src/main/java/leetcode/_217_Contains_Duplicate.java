f48b73335f39b1c91c1fe10d6177dad497c32ca4
Tai Dao <daotai221192@gmail.com>
2023-02-05 11:04:07 +0700

    Update _217_Contains_Duplicate.java

---
+++ Java 17/java/leetcode/src/main/java/leetcode/_217_Contains_Duplicate.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class _217_Contains_Duplicate {
+
+    // Cách 1
+    public boolean containsDuplicate(int[] nums) {
+        return nums.length != Arrays.stream(nums).distinct().toArray().length;
+    }
+
+    // Cách 2
+    public boolean containsDuplicate2(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length; i++) {
+            set.add(nums[i]);
+        }
+
+        return nums.length != set.size();
+    }
+}
