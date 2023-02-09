a7a7aac14e1f3d9ba161294c3308f8161b7d2ee5
Tai Dao <daotai221192@gmail.com>
2023-02-09 12:43:44 +0700

    pow

--- Java 17/java/leetcode/src/main/java/leetcode/_50_Pow.java
+++ Java 17/java/leetcode/src/main/java/leetcode/_50_Pow.java
@@ -2,20 +2,18 @@ package leetcode;
 
 public class _50_Pow {
     public static double myPow(double x, int n) {
-        if (n == 0) {
-            return 1;
-        }
+        if (n == 0) return 1;
+
         int m = 0;
         if (n < 0) {
-            m = n % 2 == 0 ? -(n / 2) : (-n - 1) / 2;
             x = 1 / x;
+            m = -(n / 2);
         } else {
-            m = n % 2 == 0 ? (n / 2) : (n - 1) / 2;
+            m = n / 2;
         }
 
         double t = myPow(x, m);
-        return n % 2 == 0 ? (t * t) : (x * t * t);
-
+        return (n % 2 == 0) ? t * t : t * t * x;
     }
 
     public static void main(String[] args) {
