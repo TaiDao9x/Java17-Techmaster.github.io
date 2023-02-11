7be7e4e882141ff65ec2435bca1450831540bdf3
Tai Dao <daotai221192@gmail.com>
2023-02-11 18:51:39 +0700

    ok

---
+++ Java 17/java/leetcode/src/main/java/leetcode/_912_Sort_an_Array.java
@@ -0,0 +1,10 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class _912_Sort_an_Array {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
