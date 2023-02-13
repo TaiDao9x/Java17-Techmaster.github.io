4d3a70ee6678feed7de5c44bddc544906f42e3dd
Tai Dao <daotai221192@gmail.com>
2023-02-13 12:21:34 +0700

    912

--- Java 17/data_structure/data_structure/day06/MyQuickSort.java
+++ Java 17/data_structure/data_structure/day06/MyQuickSort.java
@@ -4,45 +4,48 @@ import java.util.Arrays;
 
 public class MyQuickSort {
     public static void main(String[] args) {
-        int[] a = {9, 7, 5, 8, 4, 1, 2};
+        int[] a = {1, 7, 5, 8, 4, 9, 2};
         quickSort(a, 0, a.length - 1);
         System.out.println(Arrays.toString(a));
     }
 
-    public static void quickSort(int[] arr, int low, int high) {
-        if (low < high) {
-            int sortedItem = partition(arr, low, high); // tìm được 1 phần tử sorted
-            quickSort(arr, low, sortedItem - 1);
-            quickSort(arr, sortedItem + 1, high);
-
+    public static void quickSort(int[] arr, int left, int right) {
+        if (left >= right) {
+            return;
         }
+
+        int k = partition(arr, left, right);
+        quickSort(arr, left, k - 1);
+        quickSort(arr, k + 1, right);
+
     }
 
-    public static int partition(int[] arr, int low, int high) {
-        int pivot = arr[low];
-        int left = low + 1;
-        int right = high;
-        while (true) {
-            while (left <= right && arr[left] < pivot) {
-                left++; // tìm đưọc arr[left]>pivot
+    public static int partition(int[] arr, int left, int right) {
+        int key = arr[left];
+        int iL = left + 1;
+        int iR = right;
+        while (iL <= iR) {
+            while (iL < arr.length && arr[iL] < key) {
+                iL++;
             }
-            while (left <= right && arr[right] > pivot) {
-                right--; // tìm được arr[right]<pivot
+            while ( arr[iR] > key) {
+                iR--;
             }
-            if (left >= right) {
-                break;
+
+            if (iL <= iR) {
+                swap(arr, iL, iR);
+                iL++;
+                iR--;
             }
-            swap(arr, left, right);
-            left++;
-            right--;
+
         }
-        swap(arr, low, right);
-        return right;
+        swap(arr, left, iR);
+        return iR;
     }
 
-    public static void swap(int[] arr, int i, int j) {
-        int temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
+    public static void swap(int[] arr, int iL, int iR) {
+        int temp = arr[iL];
+        arr[iL] = arr[iR];
+        arr[iR] = temp;
     }
 }
--- Java 17/java/leetcode/src/main/java/leetcode/_912_Sort_an_Array.java
+++ Java 17/java/leetcode/src/main/java/leetcode/_912_Sort_an_Array.java
@@ -3,8 +3,51 @@ package leetcode;
 import java.util.Arrays;
 
 public class _912_Sort_an_Array {
-    public int[] sortArray(int[] nums) {
-        Arrays.sort(nums);
+
+    public static void main(String[] args) {
+        int[] nums = {5, 1, 1, 2, 0, 0};
+        System.out.println(Arrays.toString(sortArray(nums)));
+    }
+
+    public static int[] sortArray(int[] nums) {
+        quickSort(nums, 0, nums.length - 1);
         return nums;
+    }
+
+    public static void quickSort(int[] nums, int left, int right) {
+        if (left >= right) {
+            return;
+        }
+        int k = partition(nums, left, right);
+        quickSort(nums, left, k - 1);
+        quickSort(nums, k + 1, right);
+    }
+
+    public static int partition(int[] nums, int left, int right) {
+        int key = nums[right];
+        int iL = left;
+        int iR = right - 1;
+
+        while (iL <= iR) {
+            while (nums[iL] < key) {
+                iL++;
+            }
+            while (iR >= 0 && nums[iR] > key) {
+                iR--;
+            }
+            if (iL <= iR) {
+                swap(nums, iL, iR);
+                iL++;
+                iR--;
+            }
+        }
+        swap(nums, iL, right);
+        return iL;
+    }
+
+    public static void swap(int[] nums, int a, int b) {
+        int temp = nums[a];
+        nums[a] = nums[b];
+        nums[b] = temp;
     }
 }
