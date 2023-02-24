f91c7d04b312c3a2e09ff45ba276e69cb60d281c
Tai Dao <daotai221192@gmail.com>
2023-02-24 22:08:09 +0700

    update comment

--- Java 17/java/leetcode/src/main/java/leetcode/MinStack.java
+++ Java 17/java/leetcode/src/main/java/leetcode/MinStack.java
@@ -2,10 +2,25 @@ package leetcode;
 
 import java.util.Stack;
 
-public class MinStack {
-    Stack<Integer> stack; // Create stack to contain value
 
-    Stack<Integer> min; // Crete min to contain min value
+// Answer 1
+
+// Use 2 Stack: stack and min (save min value)
+// If stack is empty -> push val into both stack and min (the first element)
+// If not we compare val vs top of min -> if val<=min -> push val into min and stack
+// when call method pop(), we compare value need to pop vs value at top of min
+// If equal we pop both stack and min
+// getMin always is value at top of min
+
+//  stack   min
+//  [-3]
+//  [0]     [-3]
+//  [-2]    [-2]
+public class MinStack {
+
+    Stack<Integer> stack;
+
+    Stack<Integer> min;
 
 
     public MinStack() {
@@ -25,10 +40,9 @@ public class MinStack {
     }
 
     public void pop() {
-        if (stack.peek().equals(min.peek())) {
+        if (stack.pop().equals(min.peek())) {
             min.pop();
         }
-        stack.pop();
     }
 
     public int top() {
@@ -38,9 +52,55 @@ public class MinStack {
     public int getMin() {
         return min.peek();
     }
+}
 
-    //  stack   min
-    //  [-3]
-    //  [0]     [-3]
-    //  [-2]    [-2]
+
+// Answer 2
+
+// Use a stack(Stack) and a variable min(int)
+// if stack empty -> min= val and push val into stack
+// in another next push, we compare min vs val. If val<=min (we have new min)
+// -> push min into stack first, after that push val
+// when use method pop(): compare the value need to pop vs min (do pop() once)
+// If pop == min -> assign min == pop() (do pop() tiwce)
+
+//  [-3]    [0]
+//  [-2]    [0]
+//   [0]    [1]
+//  [-2]    [0]
+public class MinStack2 {
+
+    Stack<Integer> stack = new Stack<>();
+    int min = Integer.MAX_VALUE;
+
+    public MinStack() {
+    }
+
+    public void push(int val) {
+        if (stack.isEmpty()) {
+            min = val;
+        } else {
+            if (val <= min) {
+                stack.push(min);
+                min = val;
+            }
+        }
+        stack.push(val);
+    }
+
+    public void pop() {
+        if (stack.pop() == min) {
+            if (stack.isEmpty()) {
+                min = Integer.MAX_VALUE;
+            } else min = stack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return min;
+    }
 }
